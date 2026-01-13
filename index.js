d53aa4aa6254ed5b747f0feef19ae35c56ba70f9
unknown <muktinathrajbanshi786@gmail.com>
2026-01-13 21:09:58 +0545

    oh my god last nidra lagyou yar haha

---
+++ index.js
@@ -0,0 +1,5 @@
+const ourItemDiv = document.getElementsByClassName("item");
+const openIcon = document.getElementsByClassName("iconOpen");
+const closeIcon = document.getElementsByClassName("iconClose");
+
+console.log(ourItemDiv);
