4be0e75136a98a71503783c606398dc8fa56948a
unknown <muktinathrajbanshi786@gmail.com>
2026-01-13 22:06:07 +0545

    aja lai yati garum la

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const closeIcon = document.getElementsByClassName("iconClose");
 // console.log(ourItemDiv);
 
 for (let i = 0; i<ourItemDiv.length; i++) {
+
+    closeIcon[i].style.display = "none";
+
     ourItemDiv[i].addEventListener("click", () => {
         ourItemDiv[i].classList.toggle("active");
     })
