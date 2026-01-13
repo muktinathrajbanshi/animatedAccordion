8a61a412a74565f1392ee0b8bfed4af06cca9fa2
unknown <muktinathrajbanshi786@gmail.com>
2026-01-13 21:17:09 +0545

    wow nidra ko surma pani coding kasto craze hola ma coding ma

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const ourItemDiv = document.getElementsByClassName("item");
 const openIcon = document.getElementsByClassName("iconOpen");
 const closeIcon = document.getElementsByClassName("iconClose");
 
-console.log(ourItemDiv);
+// console.log(ourItemDiv);
+
+for (let i = 0; i<ourItemDiv.length; i++) {
+    ourItemDiv[i].addEventListener("click", () => {
+        ourItemDiv[i].classList.toggle("active");
+    })
+}
